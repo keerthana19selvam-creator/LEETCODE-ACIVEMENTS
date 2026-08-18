af9769e3cb3f07fcabb81ce4ec6d8ddcc7724670
KEERTHANA S <keerthana19selvam@gmail.com>
2026-08-18 19:34:08 +0530

    Implement binary search in Solution class

---
+++ solution.java
@@ -0,0 +1,21 @@
+i complete the program by this code for binary search
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+
+            if (nums[m] == target) {
+                return m;
+            } else if (nums[m] < target) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+
+        return -1;
+    }
+}
